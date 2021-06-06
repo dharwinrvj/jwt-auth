670f4a8ad60e33362ef7ee8c8679afe3b617b671
Dharwin R V J <59426893+DharwinRVJ@users.noreply.github.com>
2021-06-06 10:26:55 +0530

    eam

--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
+/*
+database for express-auth-mysql
+*/
+--
+-- database - nodelogin
+--
 CREATE DATABASE IF NOT EXISTS nodelogin;
 USE nodelogin;
+--
+-- table for users
+--
 CREATE TABLE IF NOT EXISTS `users` (
 	id int(11) NOT NULL AUTO_INCREMENT,
     name varchar(50) NOT NULL,
